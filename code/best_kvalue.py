bab38b2cec70c6b2b337047c117e9b0dac1c3bd2
Lian Choi <littlecsi27@gmail.com>
2021-04-26 14:39:36 +0100

    [add] - plots a graph

--- code/best_kvalue.py
+++ code/best_kvalue.py
@@ -1,9 +1,10 @@
 import pyupbit
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def get_ror(k=0.5):
-    df = pyupbit.get_ohlcv("KRW-BTC")
+    df = pyupbit.get_ohlcv("KRW-XRP")
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1)
 
@@ -15,7 +16,14 @@ def get_ror(k=0.5):
     ror = df['ror'].cumprod()[-2]
     return ror
 
+k_value = []
+ror = []
 
-for k in np.arange(0.1, 1.0, 0.1):
-    ror = get_ror(k)
-    print("%.1f %f" % (k, ror))
+for k in np.arange(0.0, 1.0, 0.05):
+    k_value.append(k)
+    ror.append(get_ror(k))
+
+plt.plot(k_value, ror)
+plt.xlabel("K value")
+plt.ylabel("Rate of Return")
+plt.show()
