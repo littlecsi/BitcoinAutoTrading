c030a83aa43cda071b2ea0a42b2f19168d0cb4f3
Lee <littlecsi27@gmail.com>
2021-07-26 19:02:00 +0200

    [update] minor change

--- code/test.py
+++ code/test.py
@@ -4,6 +4,7 @@ def get_balance(ticker):
     balances = upbit.get_balances()
     for b in balances:
         if b['currency'] == ticker:
+            if b['balance'] is not None:
                 return float(b['balance'])
             else:
                 return 0
