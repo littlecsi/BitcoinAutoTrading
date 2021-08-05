61be5c552ebf909f4c24e563acfad9cd96069394
Lee <littlecsi27@gmail.com>
2021-08-05 15:38:15 +0200

    [update] file name updated

--- code/bitcoinAutoTradeSingle.py
+++ code/bitcoinAutoTradeSingle.py
@@ -2,23 +2,29 @@ import time
 import pyupbit
 import datetime
 
-# import bestk
+import bestk
 
-access = "r3vDlx2fLwquOSGPM8AwA8eulChTYuSBGKarLP9o"
-secret = "lrFcol9WkkE0OBbhFIpxz3skF0sF83mESVxFKYUy"
+# access = "r3vDlx2fLwquOSGPM8AwA8eulChTYuSBGKarLP9o"
+# secret = "lrFcol9WkkE0OBbhFIpxz3skF0sF83mESVxFKYUy"
+
+access = "vTONc1AEzuBo8u3ixsDLMCp9n3q9umc9K0xStqtk"
+secret = "FxQNWmUGpALOjTUZt8igkGOcxsQNs86yimUbnRka"
 
 def get_target_price(ticker, k):
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+
     return target_price
 
 def get_start_time(ticker):
     df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
     start_time = df.index[0]
+
     return start_time
 
 def get_balance(ticker):
     balances = upbit.get_balances()
+
     for b in balances:
         if b['currency'] == ticker:
             if b['balance'] is not None:
@@ -32,30 +38,40 @@ def get_current_price(ticker):
 
 # Login
 upbit = pyupbit.Upbit(access, secret)
-print("autotrade start")
+print("---Autotrade Begin---")
 
-# k = bestk.get_k()
+
+krw_coin = "KRW-AXS"
+k = bestk.get_k(krw_coin)
 
 # AutoTrade Begin
 while True:
     try:
         now = datetime.datetime.now() + datetime.timedelta(hours=9)
-        start_time = get_start_time("KRW-XRP")
+        start_time = get_start_time(krw_coin)
         end_time = start_time + datetime.timedelta(days=1)
 
+        print(now)
+
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-XRP", 0.5)
-            current_price = get_current_price("KRW-XRP")
+            target_price = get_target_price(krw_coin, k)
+            current_price = get_current_price(krw_coin)
+
             if target_price < current_price:
                 krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-XRP", krw*0.9995)
-        else:
-            coin = get_balance("XRP")
-            curr_coin = get_current_price("KRW-XRP")
 
-            if (coin * curr_coin) > 5000:
-                upbit.sell_market_order("KRW-XRP", coin*0.9995)
+                if krw > 5000:
+                    upbit.buy_market_order(krw_coin, krw*0.9995)
+                    print("purchased")
+        else:
+            bal = get_balance(krw_coin[4:])
+            curr_coin = get_current_price(krw_coin)
+
+            if (bal * curr_coin) > 5000:
+                upbit.sell_market_order(krw_coin, bal*0.9995)
+                print("sold")
+
+            k = bestk.get_k(krw_coin)
 
         time.sleep(1)
 
