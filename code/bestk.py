307063929d6300d621e03f19c06c54ba9acaa6b5
Lee <littlecsi27@gmail.com>
2021-08-08 20:39:45 +0200

    [update] code clean-up

--- code/bestk.py
+++ code/bestk.py
@@ -1,7 +1,5 @@
 import pyupbit
 import numpy as np
-# import matplotlib.pyplot as plt
-
 
 def get_ror(coin_name, k = 0.5):
     df = pyupbit.get_ohlcv(coin_name)
@@ -21,14 +19,8 @@ def get_k(coin_name):
     k_value = []
     ror = []
 
-    for k in np.arange(0.1, 1.0, 0.2):
+    for k in np.arange(0.2, 1.0, 0.2):
         k_value.append(k)
         ror.append(get_ror(coin_name, k))
 
     return k_value[ror.index(max(ror))]
-
-# Plotting
-# plt.plot(k_value, ror)
-# plt.xlabel("K value")
-# plt.ylabel("Rate of Return")
-# plt.savefig('plot.png', dpi=300, bbox_inches='tight')
