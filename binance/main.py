f38c8bb4002a339f156b0ffd0d16ec69a98cc16c
Lee <littlecsi27@gmail.com>
2022-08-02 15:46:18 +0200

    [add] implmeneted Volatile Breakout Strategy
    This code is ready to be integrated onto AWS server

--- binance/main.py
+++ binance/main.py
@@ -5,22 +5,15 @@ from binance.spot import Spot
 
 import time
 import datetime
-import pprint
 
 asset = "ETHUSDT"
+balance = 0.0
 target_price = 0.0
 
 # api key/secret are required for user data endpoints
 client = Spot(key=config.api_key, secret=config.api_secret)
 
 def main():
-    # Get account and balance information
-    print("Current USDT balance :", biat.get_balance(client))
-    print("Current ETH price :", biat.get_current_price(client, "ETH"))
-
-    print(biat.get_ytd_ohlcv(client, asset))
-    biat.get_tdy_ohlcv(client, asset)
-
     now = datetime.datetime.utcnow()
     mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
 
@@ -29,26 +22,28 @@ def main():
     while True:
         now = datetime.datetime.utcnow()
         
+        # When the day ends
         if mid < now < mid + datetime.timedelta(seconds=10):
+            # Calculate new target price for the next day.
             target_price = biat.get_target_price(client, asset)
+
+            # Update next day
             mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
 
+            # Sell crypto
+            biat.sell_crypto(client, biat.get_balance(client, asset), asset)
+
         current_price = biat.get_current_price(client, asset)
+
+        # If the current price reaches the target price
+        if current_price >= target_price:
+            # Get the entire balance of USDT dollars
+            balance = biat.get_balance(client, "USDT")
+
+            # Purchase the maximum amount of crypto user can order.
+            biat.buy_crypto(client, balance, target_price, asset)
 
         time.sleep(10)
 
 if __name__ == "__main__":
     main()
-    
-"""# Post a new order
-params = {
-    'symbol': 'BTCUSDT',
-    'side': 'SELL',
-    'type': 'LIMIT',
-    'timeInForce': 'GTC',
-    'quantity': 0.002,
-    'price': 9500
-}
-
-response = client.new_order(**params)
-print(response)""" 
